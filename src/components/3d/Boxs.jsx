bb75c0ed86cb6d6ca1039259246e45806002e7f7
273032860 <273032860@qq.com>
2025-12-11 15:13:40 +0800

    threejs 升级到 182 ，添加新 ui

--- src/components/3d/Boxs.jsx
+++ src/components/3d/Boxs.jsx
@@ -1,6 +1,8 @@
 import { Box } from '@react-three/drei'
-import React from 'react'
+import React, { useMemo } from 'react'
 import { useAppStore } from '@/Store'
+import { Color } from 'three'
+import { Fn, uniform } from 'three/tsl'
 
 const Boxs = () => {
   const toggleLeftTop = useAppStore((state)=>state.toggleLeftTop)
@@ -14,9 +16,31 @@ const Boxs = () => {
   function onClick2() {
     setLeftBottom(false)
   }
+
+  const { nodes } = useMemo(() => {
+  const time = uniform(0.0);
+  const color1 = uniform(new Color(0xff0000), 'color');
+   
+
+  const colorNode = Fn(() => {
+    return color1
+  })();
+
+  return {
+    nodes: {
+       
+      colorNode,
+    },
+    uniforms: {
+      time,
+    },
+  }
+}, [])
   return (
     <>
-      <Box onClick={onClick}/>
+      <Box onClick={onClick}>
+        <meshBasicMaterial {...nodes} />
+        </Box>
       <Box position={[4,0,0]} onClick={onClick1}/>
       <Box position={[4,2,0]} onClick={onClick2}/>
     </>
--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { dirname, resolve } from 'path'
 import { fileURLToPath } from 'url'
 import { reactGrab } from "react-grab/plugins/vite";
 import { codeInspectorPlugin } from 'code-inspector-plugin';
+import threeUniformGui from 'tsl-uniform-ui-vite-plugin'
 
 
 const __filename = fileURLToPath(import.meta.url)
@@ -13,12 +14,18 @@ const __dirname = dirname(__filename)
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    threeUniformGui({ 
+      devOnly: true,     // 仅在开发模式下活跃
+      persistent: true,   //在本地存储中保存配置
+      // presets: true,      // 启用预设管理
+      draggable: true,    // 启用拖拽
+    }), // ✅ BEFORE react()
     react(),
     tailwindcss(),
     reactGrab(),
-    codeInspectorPlugin({
-      bundler: 'vite',
-    }),
+    // codeInspectorPlugin({
+    //   bundler: 'vite',
+    // }),
   ],
   resolve: {
     alias: {
